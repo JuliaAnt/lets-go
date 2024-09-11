62f0c47e78ec5b03a2f257511d48f2d6562f0186
rmoongit <emil_tt@mail.ru>
2024-09-12 02:08:37 +0300

    feat: добавляет Header/Desktop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,57 @@
-import './Header.scss';
-import logo from '../../assets/logo_full.svg';
+import styles from './header.module.scss';
 
-export const Header = () => {
+import logo from '../../assets/logo_full.svg';
+import iconCall from '../../assets/icons/header/icon-call.svg';
+import iconMail from '../../assets/icons/header/icon-mail.svg';
+
+export const Header = ({title = 'Направления'}) => {
   return (
-    <header className='header'>
-      <img src={logo} alt='logo' />
-      <p>Header</p>
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerWrapper}>
+          <a className={styles.headerLogo} href="#">
+            <img src={logo} alt="logo" />
+          </a>
+
+          <div className={styles.headerLine}></div>
+
+          <nav className={styles.nav}>
+            <ul className={`${styles.navList} ${styles.navListFirst}`}>
+              <li>
+                <a href="#">
+                  <span data-hover="О СЕРВИСЕ">о сервисе</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span data-hover="НАПРАВЛЕНИЯ">направления</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span data-hover="ПОПУТЧИКИ">попутчики</span>
+                </a>
+              </li>
+            </ul>
+
+            <ul className={`${styles.navList} ${styles.navListSecond}`}>
+              <li>
+                <a href="#">
+                  <img src={iconCall} width="16" height="16" alt="phone_img" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={iconMail} width="16" height="16" alt="mail_img" />
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <p className={styles.headerTitle}>{title}</p>
+      </div>
     </header>
   );
 };
+
+export default Header;
