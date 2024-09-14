8bc4035ce0f8d705b846bda36426ebcf6034c2fa
rmoongit <emil_tt@mail.ru>
2024-09-15 00:08:19 +0300

    feat: добавялет и стилизует footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,18 @@
-import './Footer.scss';
+import styles from './footer.module.scss';
+
+import {Wrapper} from '../Wrapper/Wrapper';
+import {Logo} from './Logo/Logo';
+import {Navigation} from './Navigation/Navigation';
 
 export const Footer = () => {
-  return <footer className='footer'>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <Wrapper>
+        <div className={styles.footerWrapper}>
+          <Logo />
+          <Navigation />
+        </div>
+      </Wrapper>
+    </footer>
+  );
 };
