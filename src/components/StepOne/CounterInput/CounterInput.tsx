6222ecc6077c493cb01c712fbce76a01e1ba96c7
Julia <julia.antoshkina@gmail.com>
2024-09-14 12:30:00 +0200

    feat: добавляет стейт для хранения количества попутчиков и длительности поездки

---
+++ src/components/StepOne/CounterInput/CounterInput.tsx
@@ -0,0 +1,93 @@
+import { ChangeEvent, useState } from 'react'
+import { ReactComponent as IconMinus } from '../../../assets/minus.svg'
+import { ReactComponent as IconPlus } from '../../../assets/plus.svg'
+import styles from '../StepOne.module.scss'
+
+interface CounterInputProps {
+  data: {
+    id: string
+    label: string
+    min: number
+    max: number
+    unit: string
+  }
+  amount: number
+  className?: string
+  setAmount: (amount: number) => void
+}
+
+export const CounterInput = ({ data, amount, className, setAmount }: CounterInputProps) => {
+  const [inputValue, setInputValue] = useState<string>(amount.toString())
+
+  const onIncrementCount = () => {
+    if (amount === data.max) {
+      setAmount(data.max)
+    } else {
+      setAmount(amount + 1)
+    }
+  }
+
+  const onDecrementCount = () => {
+    if (amount === data.min) {
+      setAmount(data.min)
+    } else {
+      setAmount(amount - 1)
+    }
+  }
+
+  const handleAmountChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(evt.target.value)
+  }
+
+  const handleBlur = () => {
+    let newValue = parseFloat(inputValue)
+    if (isNaN(newValue)) {
+      newValue = data.min
+    } else if (newValue < data.min) {
+      newValue = data.min
+    } else if (newValue > data.max) {
+      newValue = data.max
+    }
+    setAmount(newValue)
+    setInputValue(newValue.toString())
+  }
+
+  const handleKeyDown = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+    if (evt.key === 'Enter') {
+      handleBlur()
+    }
+  }
+
+  return (
+    <label className={`${styles.numberInput} ${className ? className : ''}`} htmlFor={data.id}>
+      <span>{data.label}:</span>
+      <span className={styles.inputInner}>
+        <button
+          className={styles.minusButton}
+          type='button'
+          aria-label='Минус один'
+          onClick={onDecrementCount}
+        >
+          <IconMinus />
+        </button>
+        <input
+          type='number'
+          id={data.id}
+          value={inputValue}
+          onChange={(evt) => handleAmountChange(evt)}
+          onBlur={handleBlur}
+          onKeyDown={handleKeyDown}
+        />
+        <button
+          className={styles.plusButton}
+          type='button'
+          aria-label='Плюс один'
+          onClick={onIncrementCount}
+        >
+          <IconPlus />
+        </button>
+      </span>
+      <span className={styles.labelData}>{data.unit}</span>
+    </label>
+  )
+}
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -4,7 +4,24 @@ export enum AppRoute {
 }
 
 export const NAVIGATION_MAP = [
-  {text: 'о сервисе', hoverText: 'О СЕРВИСЕ'},
-  {text: 'направления', hoverText: 'НАПРАВЛЕНИЯ'},
-  {text: 'попутчики', hoverText: 'ПОПУТЧИКИ'},
-];
+  { text: 'о сервисе', hoverText: 'О СЕРВИСЕ' },
+  { text: 'направления', hoverText: 'НАПРАВЛЕНИЯ' },
+  { text: 'попутчики', hoverText: 'ПОПУТЧИКИ' },
+]
+
+export const COUNTER_INPUT_MAP = {
+  travelerCounter: {
+    id: 'travelerCounter',
+    label: 'ищу попутчиков',
+    min: 1,
+    max: 10,
+    unit: 'чел.',
+  },
+  travelLength: {
+    id: 'travelLength',
+    label: 'длительность',
+    min: 2,
+    max: 31,
+    unit: 'дн.',
+  },
+}
