1139eaecdeda6e9ab6317628df89aabfb1e31ca4
Ksu <ksenya-7@yandex.ru>
2024-09-16 20:36:04 +0300

    fix: добавляет ПП пагинации

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,11 +1,16 @@
-import {Footer} from '../../components/Footer/Footer';
 import {Header} from '../../components/Header/Header';
+import {Footer} from '../../components/Footer/Footer';
+import {Pagination} from "../../components/Pagination/Pagination";
+
 
 export const CatalogPage = () => {
   return (
     <div className='wrapper'>
       <Header />
-      <main className='catalog-page'>Попутчики</main>
+      <main className='catalog-page'>
+        <Pagination/>
+        
+      </main>
       <Footer />
     </div>
   );
