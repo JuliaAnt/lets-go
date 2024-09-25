3138c4f41be0d9d888f6d2b4a6d0f7dd6e4f79b2
Ksu <ksenya-7@yandex.ru>
2024-09-25 22:50:51 +0300

    fix: добавляет ПП частично для планшета

--- src/vendor/init-nouislider.js
+++ src/vendor/init-nouislider.js
@@ -1,11 +1,5 @@
 import noUiSlider from './nouislider.js'
 
-// let slider = document.querySelector('[data-slider-duration]');
-// console.log(slider);
-
-// const submitButton = document.querySelector('[data-submit-filters-button]');
-// const resetButton = document.querySelector('[data-reset-filters-button]');
-
 export default function createUiSlider(slider) {
   if (!noUiSlider) {
     console.error('Ошибка: noUiSlider не найден. ')
@@ -20,8 +14,8 @@ export default function createUiSlider(slider) {
   }
 
   function mergeTooltips(slider, threshold, separator) {
-    let textIsRtl = getComputedStyle(slider).direction === 'rtl'
-    let isRtl = slider.noUiSlider.options.direction === 'rtl'
+    // let textIsRtl = getComputedStyle(slider).direction === 'rtl'
+    // let isRtl = slider.noUiSlider.options.direction === 'rtl'
     let isVertical = slider.noUiSlider.options.orientation === 'vertical'
     let tooltips = slider.noUiSlider.getTooltips()
     let origins = slider.noUiSlider.getOrigins()
@@ -33,39 +27,39 @@ export default function createUiSlider(slider) {
       }
     })
 
-    slider.noUiSlider.on('update', function (values, handle, unencoded, tap, positions) {
+    slider.noUiSlider.on('update', function (values, handle, unencoded, tap) {
       let pools = [[]]
-      let poolPositions = [[]]
+      // let poolPositions = [[]]
       let poolValues = [[]]
-      let atPool = 0
+      // let atPool = 0
 
       // Assign the first tooltip to the first pool, if the tooltip is configured
       if (tooltips[0]) {
         pools[0][0] = 0
-        poolPositions[0][0] = positions[0]
+        // poolPositions[0][0] = positions[0]
         poolValues[0][0] = parseFloat(values[0])
           .toFixed(0)
           .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
       }
 
-      for (let i = 1; i < positions.length; i++) {
-        if (!tooltips[i] || positions[i] - positions[i - 1] > threshold) {
-          atPool++
-          pools[atPool] = []
-          poolValues[atPool] = []
-          poolPositions[atPool] = []
-        }
+      // for (let i = 1; i < positions.length; i++) {
+      //   if (!tooltips[i] || positions[i] - positions[i - 1] > threshold) {
+      //     atPool++
+      //     pools[atPool] = []
+      //     poolValues[atPool] = []
+      //     poolPositions[atPool] = []
+      //   }
 
-        if (tooltips[i]) {
-          pools[atPool].push(i)
-          poolValues[atPool].push(
-            parseFloat(values[i])
-              .toFixed(0)
-              .replace(/\B(?=(\d{3})+(?!\d))/g, ' '),
-          )
-          poolPositions[atPool].push(positions[i])
-        }
-      }
+      //   if (tooltips[i]) {
+      //     pools[atPool].push(i)
+      //     poolValues[atPool].push(
+      //       parseFloat(values[i])
+      //         .toFixed(0)
+      //         .replace(/\B(?=(\d{3})+(?!\d))/g, ' '),
+      //     )
+      //     poolPositions[atPool].push(positions[i])
+      //   }
+      // }
 
       pools.forEach(function (pool, poolIndex) {
         let handlesInPool = pool.length
@@ -74,21 +68,21 @@ export default function createUiSlider(slider) {
           let handleNumber = pool[j]
 
           if (j === handlesInPool - 1) {
-            let offset = 0
+            // let offset = 0
 
-            poolPositions[poolIndex].forEach(function (value) {
-              offset += 1000 - value
-            })
+            // poolPositions[poolIndex].forEach(function (value) {
+            //   offset += 1000 - value
+            // })
 
-            let direction = isVertical ? 'bottom' : 'right'
-            let last = isRtl ? 0 : handlesInPool - 1
-            let lastOffset = 1000 - poolPositions[poolIndex][last]
-            offset = (textIsRtl && !isVertical ? 100 : 0) + offset / handlesInPool - lastOffset
+            // let direction = isVertical ? 'bottom' : 'right'
+            // let last = isRtl ? 0 : handlesInPool - 1
+            // let lastOffset = 1000 - poolPositions[poolIndex][last]
+            // offset = (textIsRtl && !isVertical ? 100 : 0) + offset / handlesInPool - lastOffset
 
             // Center this tooltip over the affected handles
             tooltips[handleNumber].innerHTML = poolValues[poolIndex].join(separator)
             tooltips[handleNumber].style.display = 'block'
-            tooltips[handleNumber].style[direction] = offset + '%'
+            // tooltips[handleNumber].style[direction] = offset + '%'
           } else {
             // Hide this tooltip
             tooltips[handleNumber].style.display = 'none'
@@ -106,27 +100,11 @@ export default function createUiSlider(slider) {
       tooltips: [true, true],
       range: {
         min: 1,
-        '30%': [30, 100],
+        // '30%': [30, 100],
         max: 100,
       },
     })
 
     mergeTooltips(slider, 10, ' - ')
   }
-
-  // function resetSlider() {
-  //   setTimeout(() => {
-  //     if (slider) {
-  //       slider.noUiSlider.reset();
-  //     }
-  //   }, 300);
-  // }
-
-  // if (submitButton) {
-  //   submitButton.addEventListener('click', resetSlider);
-  // }
-
-  // if (resetButton) {
-  //   resetButton.addEventListener('click', resetSlider);
-  // }
 }
