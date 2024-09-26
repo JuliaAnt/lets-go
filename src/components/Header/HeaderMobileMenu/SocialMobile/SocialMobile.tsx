3297510bd5e7201927c67c3382371ce2817840eb
rmoongit <emil_tt@mail.ru>
2024-09-26 20:31:46 +0300

    fix: правит пп

--- src/components/Header/HeaderMobileMenu/SocialMobile/SocialMobile.tsx
+++ src/components/Header/HeaderMobileMenu/SocialMobile/SocialMobile.tsx
@@ -1,29 +1,29 @@
-import {Link} from 'react-router-dom';
-import styles from './socialMobile.module.scss';
-import iconTelegram from '../../../../assets/icons/header/telegram.svg';
-import iconVkontakte from '../../../../assets/icons/header/vkontakte.svg';
-import iconYoutube from '../../../../assets/icons/header/youtube.svg';
+import { Link } from 'react-router-dom'
+import styles from './socialMobile.module.scss'
+import iconTelegram from '../../../../assets/icons/header/telegram.svg'
+import iconVkontakte from '../../../../assets/icons/header/vkontakte.svg'
+import iconYoutube from '../../../../assets/icons/header/youtube.svg'
 
 export const SocialMobile = () => {
   return (
     <nav className={styles.footer}>
-      <ul>
+      <ul className={styles.linksList}>
         <li>
-          <Link to="#">
-            <img src={iconTelegram} width={20} height={18} alt="Telegram" />
+          <Link to='#'>
+            <img src={iconTelegram} width={20} height={18} alt='Telegram' />
           </Link>
         </li>
         <li>
-          <Link to="#">
-            <img src={iconVkontakte} width={30} height={16} alt="VK" />
+          <Link to='#'>
+            <img src={iconVkontakte} width={30} height={16} alt='VK' />
           </Link>
         </li>
         <li>
-          <Link to="#">
-            <img src={iconYoutube} width={23} height={23} alt="Youtube" />
+          <Link to='#'>
+            <img src={iconYoutube} width={23} height={23} alt='Youtube' />
           </Link>
         </li>
       </ul>
     </nav>
-  );
-};
+  )
+}
