fa03dce6df3e1d4b9c6199bb5d470cd25dd62878
Hopperys <vano_ust.kz@mail.ru>
2024-10-14 22:56:10 +0500

    Правит баги

--- src/components/IntroForm/IntroCatalog.tsx
+++ src/components/IntroForm/IntroCatalog.tsx
@@ -3,8 +3,10 @@ import styles from './IntroForm.module.scss'
 export const IntroCatalog = () => {
   return (
     <section className={`${styles.introForm} ${styles.introCatalog}`}>
-      <div className={`${styles.top} ${styles.catalogTop}`}>
-        <h1>Попутчики</h1>
+      <div className={`${styles.topWrapper}`}>
+        <div className={`${styles.top} ${styles.catalogTop}`}>
+          <h1>Попутчики</h1>
+        </div>
       </div>
     </section>
   )
